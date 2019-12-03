50f14f7d591aa97964f89e0d13f700a0ef34183c
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-12-03 19:54:01 +0100

    Added online / offline status for users in a group

--- src/Components/OnlineUsers/index.js
+++ src/Components/OnlineUsers/index.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PresenceActiveIcon from '@atlaskit/icon/glyph/presence-active';
+import api from '../../services/api';
 
 import { Container, UserContainer, AvatarPhoto } from './styles';
 
 const OnlineUsers = ({ onlineUsers }) => {
+  const [allUsers, setAllUsers] = useState([]);
 
-  const users = onlineUsers.map(user => {
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      const response = await api.get(`/users`);
+      const users = response.data.users.map(user => {
+        return {
+          username: user.username,
+          photo: `https://eu.ui-avatars.com/api/?name=${user.username.substring(0, 2)}`,
+          online: false
+        }
+      });
+      setAllUsers(defineUsersStatus(users, onlineUsers));
+    }
+    fetchAllUsers();
+  }, [onlineUsers]);
+
+  /**
+   * Merges both users lists, returning a list with the users status inside the chat
+   * @param allUsers List of all users inside a group 
+   * @param onlineUsersList List of online users inside a group
+   */
+  const defineUsersStatus = (allUsers, onlineUsersList) => {
+    return [...allUsers.map(offlineUser => {
+      onlineUsersList.map(onlineUser => {
+        if (offlineUser.username === onlineUser.username) {
+          offlineUser.online = true;
+        } else {
+          offlineUser.online = false;
+        }
+        return offlineUser;
+      })
+      return offlineUser;
+    })];
+  }
+
+  const users = allUsers.map(user => {
     return (
-      <UserContainer>
+      <UserContainer key={user.username}>
         <span>
-          <PresenceActiveIcon 
+          <PresenceActiveIcon
             size="small"
-            primaryColor="green"/>
+            primaryColor={user.online ? 'green' : 'grey'} />
         </span>
-        <AvatarPhoto photo={user.photo}/>
+        <AvatarPhoto photo={user.photo} />
         <p>{user.username}</p>
       </UserContainer>
     )
   });
-  
+
   return (
     <Container>
       <h3>FRIENDS</h3>
